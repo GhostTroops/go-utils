ea79e9804ccb0733245f068b99556e45cfbeea4d
hktalent <18223385+hktalent@users.noreply.github.com>
2022-10-16 02:25:33 +0800

    加了 N 进制逆向计算； 同时调整 62 进制混乱顺序，避免其他人容易破解 2022-10-16

--- strTools.go
+++ strTools.go
@@ -1,5 +1,52 @@
 package go_utils
 
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+var Tplat = "ab9cdef8ghijk0lmnopqr1stuvw2xyzAB3CDEFGHI4JKLMN5OPQRS6TUVW7XYZ"
+
+// 随机模版
+func GetRadomTemplate() string {
+	a := GenerateRandomNumber(0, 62, 62)
+	var b = ""
+	for _, x := range a {
+		b += Tplat[x : x+1]
+	}
+	return b
+}
+
+// 生成count个[start,end)结束的不重复的随机数
+//  可以在一次会话中随机生成62个数字，构建 62 进制字符串模版
+func GenerateRandomNumber(start int, end int, count int) []int {
+	// 范围检查
+	if end < start || (end-start) < count {
+		return nil
+	}
+	// 存放结果的slice
+	nums := make([]int, 0)
+	//随机数生成器，加入时间戳保证每次生成的随机数不一样
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for len(nums) < count {
+		// 生成随机数
+		num := r.Intn((end - start)) + start
+		// 查重
+		exist := false
+		for _, v := range nums {
+			if v == num {
+				exist = true
+				break
+			}
+		}
+		if !exist {
+			nums = append(nums, num)
+		}
+	}
+	return nums
+}
+
 // 将十进制转换为 任意进制,需要注意的是，域名总不能有 下划线(_)，但是可以有减号(-)
 // 0 -- > 0
 // 1 -- > 1
@@ -26,7 +73,24 @@ func TransInt64ToN(id int64, szTemplate string) string {
 	return string(shortUrl)
 }
 
+// N 进制逆向计算
+func TransN2Int64(str string, szTemplate string) int64 {
+	n := int64(len(szTemplate))
+	nR := int64(0)
+	for i := 0; i < len(str); i++ {
+		x := strings.Index(szTemplate, str[i:i+1])
+		nR = nR * n
+		nR += int64(x)
+	}
+	return nR
+}
+
+// 字符串还原数字
+func Trans62ToInt64(str string) int64 {
+	return TransN2Int64(str, Tplat)
+}
+
 // 数字转 62 进制
 func TransInt64To62(id int64) string {
-	return TransInt64ToN(id, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	return TransInt64ToN(id, Tplat)
 }
--- strTools_test.go
+++ strTools_test.go
@@ -1,6 +1,8 @@
 package go_utils
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestTransInt64To62(t *testing.T) {
 	type args struct {
@@ -11,17 +13,75 @@ func TestTransInt64To62(t *testing.T) {
 		args args
 		want string
 	}{
-		{"测试62进制转换 1", args{889533}, "3Jpj"},
-		{"测试62进制转换 2", args{11}, "b"},
-		{"测试62进制转换 3", args{0}, "0"},
-		{"测试62进制转换 4", args{10}, "a"},
-		{"测试62进制转换 5", args{62}, "10"},
+		{"测试62进制转换 1", args{889533}, "cMvq"},
+		{"测试62进制转换 2", args{11}, "j"},
+		{"测试62进制转换 3", args{0}, "a"},
+		{"测试62进制转换 4", args{10}, "i"},
+		{"测试62进制转换 5", args{62}, "ba"},
 		{"测试62进制转换 6", args{61}, "Z"},
+		{"测试62进制转换 7", args{0xff}, "d8"},
 	}
+	var aR []string
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := TransInt64To62(tt.args.id); got != tt.want {
 				t.Errorf("TransInt64To62() = %v, want %v", got, tt.want)
+			} else {
+				aR = append(aR, got)
+			}
+		})
+	}
+
+	type args2 struct {
+		id string
+	}
+	tests1 := []struct {
+		name string
+		args args2
+		want int64
+	}{
+		{"fz 1", args2{aR[0]}, 889533},
+		{"fz 2", args2{aR[1]}, 11},
+		{"fz 3", args2{aR[2]}, 0},
+		{"fz 4", args2{aR[3]}, 10},
+		{"fz 5", args2{aR[4]}, 62},
+		{"fz 6", args2{aR[5]}, 61},
+		{"fz 7", args2{aR[6]}, 255},
+	}
+	for _, tt := range tests1 {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Trans62ToInt64(tt.args.id); got != tt.want {
+				t.Errorf("TransInt64To62() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetRadomTemplate(t *testing.T) {
+	type args struct {
+		id int64
+	}
+	szTp := GetRadomTemplate()
+	t.Log("template: ", szTp)
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"测试62进制转换 1", args{889533}, "cMvq"},
+		{"测试62进制转换 2", args{11}, "j"},
+		{"测试62进制转换 3", args{0}, "a"},
+		{"测试62进制转换 4", args{10}, "i"},
+		{"测试62进制转换 5", args{62}, "ba"},
+		{"测试62进制转换 6", args{61}, "Z"},
+		{"测试62进制转换 7", args{0xff}, "d8"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TransInt64ToN(tt.args.id, szTp); tt.args.id != TransN2Int64(got, szTp) {
+				t.Errorf("TransInt64To62() = %v, want %v", got, tt.args.id)
+			} else {
+				t.Logf("%d <= to => %s is ok", tt.args.id, got)
 			}
 		})
 	}
