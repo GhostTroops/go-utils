000790be0266f42e317a6cff64d0963ff4f9365a
hktalent <18223385+hktalent@users.noreply.github.com>
2022-11-30 11:32:39 +0800

    add SignalCandidate 2022-11-30

--- e2eTools.go
+++ e2eTools.go
@@ -1,9 +1,30 @@
 package go_utils
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/pion/webrtc/v3"
 	"log"
+	"net/http"
 )
+
+const E2ePath = "/e2eRelay"
+
+// 发送通讯信号
+func SignalCandidate(addr string, c *webrtc.ICECandidate) error {
+	payload := []byte(c.ToJSON().Candidate)
+	resp, err := http.Post(fmt.Sprintf("http://%s%s", addr, E2ePath), // nolint:noctx
+		"application/json; charset=utf-8", bytes.NewReader(payload))
+	if err != nil {
+		return err
+	}
+
+	if closeErr := resp.Body.Close(); closeErr != nil {
+		return closeErr
+	}
+
+	return nil
+}
 
 func GetPeerConnection() *webrtc.PeerConnection {
 	config := webrtc.Configuration{
