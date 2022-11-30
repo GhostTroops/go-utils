3cb94e2551be253387312b62ed7d2056daa13407
hktalent <18223385+hktalent@users.noreply.github.com>
2022-11-30 12:36:43 +0800

    add Decode/Encode 2022-11-30

--- e2eTools.go
+++ e2eTools.go
@@ -2,13 +2,20 @@ package go_utils
 
 import (
 	"bytes"
+	"compress/gzip"
+	"encoding/base64"
 	"fmt"
 	"github.com/pion/webrtc/v3"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-const E2ePath = "/e2eRelay"
+const (
+	E2ePath = "/e2eRelay"
+	// Allows compressing offer/answer to bypass terminal input limits.
+	compress = true
+)
 
 // 发送通讯信号
 func SignalCandidate(addr string, c *webrtc.ICECandidate) error {
@@ -42,4 +49,72 @@ func GetPeerConnection() *webrtc.PeerConnection {
 		return nil
 	}
 	return peerConnection
+}
+
+// Encode encodes the input in base64
+// It can optionally zip the input before encoding
+func Encode(obj interface{}) string {
+	b, err := json.Marshal(obj)
+	if err != nil {
+		panic(err)
+	}
+
+	if compress {
+		b = zip(b)
+	}
+
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+// Decode decodes the input from base64
+// It can optionally unzip the input after decoding
+func Decode(in string, obj interface{}) {
+	b, err := base64.StdEncoding.DecodeString(in)
+	if err != nil {
+		panic(err)
+	}
+
+	if compress {
+		b = unzip(b)
+	}
+
+	err = json.Unmarshal(b, obj)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func zip(in []byte) []byte {
+	var b bytes.Buffer
+	gz := gzip.NewWriter(&b)
+	_, err := gz.Write(in)
+	if err != nil {
+		panic(err)
+	}
+	err = gz.Flush()
+	if err != nil {
+		panic(err)
+	}
+	err = gz.Close()
+	if err != nil {
+		panic(err)
+	}
+	return b.Bytes()
+}
+
+func unzip(in []byte) []byte {
+	var b bytes.Buffer
+	_, err := b.Write(in)
+	if err != nil {
+		panic(err)
+	}
+	r, err := gzip.NewReader(&b)
+	if err != nil {
+		panic(err)
+	}
+	res, err := ioutil.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+	return res
 }
